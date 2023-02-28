5d68331e589909bdc0655c3bcc1b3eba03c02845
Solly Stinchcombe <solomonbooth63@gmail.com>
2023-02-28 12:21:37 +0000

    Initial Commit

---
+++ app/src/main/java/com/sollystinchcombe/mortagagecalculator/MainActivity.java
@@ -0,0 +1,22 @@
+package com.sollystinchcombe.mortagagecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("file:///android_asset/index.html");
+
+    }
+}
